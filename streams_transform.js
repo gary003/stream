5a9d124e90a51451356ae7a35f5c617895109cec
gary003 <gary.johnson003@gmail.com>
2022-12-26 10:17:07 +0100

    update streams

--- streams_transform.js
+++ streams_transform.js
@@ -4,25 +4,30 @@
 
 /* !!!!! spawn uses streams and no shell (good security),
    while exec uses a buffer , a shell and a lot of memory */
-const { spawn } = require("child_process")
 const { pipeline } = require("stream")
-
-const options = ["./files/lorem.txt"]
-const less = spawn("less", options)
 const fs = require("fs")
+
+const fileToRead = "./files/lorem.txt"
+
+const readStream = fs.createReadStream(fileToRead)
 
 /* beware , the key word 'yield' must be in a 'for of' loop 
    not a '.forEach(() => ...)' */
 const transform = async function* (source) {
   for await (const val of source) {
+    // console.log(val.toString())
     const vv = val.toString("utf8").split(" ")
-    for (const v of vv) yield v.toString()
+    // console.log(vv)
+    for (const v of vv) {
+      // console.log(v)
+      yield v + ":"
+    }
   }
 }
 
 /* !! to use an async function as a transform (made with a generator) stream ,
    you must use 'pipeline' not 'pipe' */
-pipeline(less.stdout, transform, process.stdout, (err) => console.error(err))
+pipeline(readStream, transform, process.stdout, (err) => console.error(err))
 
 // ----------------------------
 //  -----   old way   ---------
@@ -31,12 +36,13 @@ const through = require("through2")
 
 // You can't use the ES6 syntaxe because of the keyword 'this'
 const transformStream = through(function (chunk, encoding, next) {
-  this.push(`-- ${chunk}`)
+  // console.log(chunk.toString())
+  this.push(`-- ${chunk.toString()}`)
   next()
 })
 
-const readS = fs.createReadStream("/home/gary/devs/dev_manips/files/lorem.txt")
-const writeS = fs.createWriteStream("/home/gary/devs/dev_manips/files/lolorem.txt")
+const readS = fs.createReadStream("./files/lorem.txt")
+const writeS = fs.createWriteStream("./files/lolorem.txt")
 
 readS.pipe(transformStream).pipe(writeS)
 
