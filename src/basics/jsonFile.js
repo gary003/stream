2eec63b9eb316306ee7c49b31ef8f9c2479221e3
gary003 <gary.johnson003@gmail.com>
2023-01-09 07:40:27 +0100

    2 ways of handling the string conversion in streams

--- src/basics/jsonFile.js
+++ src/basics/jsonFile.js
@@ -1,13 +1,21 @@
 const JSONStream = require("JSONStream")
 const fs = require("node:fs")
-const { pipeline } = require("stream")
+const { pipeline } = require("node:stream")
 const through = require("through2")
 
 const streamFile = fs.createReadStream("./src/files/post.json", { encoding: "utf8" })
 const parser = JSONStream.parse("posts.*")
 
-pipeline(streamFile, parser, through.obj(write), process.stdout, (err) => console.error(err))
+// with async generator
+pipeline(streamFile, parser, transf, process.stdout, (err) => console.error(err))
+async function* transf(source) {
+  for await (chunk of source) {
+    yield JSON.stringify(chunk)
+  }
+}
 
+// With through.obj
+pipeline(streamFile, parser, through.obj(write), process.stdout, (err) => console.error(err))
 function write(row, enc, next) {
-  next(null, JSON.stringify(row) + "\n")
+  next(null, JSON.stringify(row))
 }
