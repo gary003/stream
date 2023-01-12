e5ffdeda8b4faa251a84768122f787f43c2b19aa
gary003 <gary.johnson003@gmail.com>
2023-01-12 13:05:28 +0100

    use of json !! carefull premature error with pipeline

--- src/streamDoc/jsonFile.js
+++ src/streamDoc/jsonFile.js
@@ -7,6 +7,7 @@ const streamFile = fs.createReadStream("./src/files/post.json", { encoding: "utf
 const parser = JSONStream.parse("posts.*")
 
 // with async generator
+// using pipeline can cause premature error... (even if its working fine ...)
 pipeline(streamFile, parser, transf, process.stdout, (err) => console.error(err))
 async function* transf(source) {
   for await (chunk of source) {
@@ -14,8 +15,20 @@ async function* transf(source) {
   }
 }
 
-// With through.obj
+// -----------   With through.obj ---------------
+// using pipeline can cause premature error... (even if its working fine ...)
 pipeline(streamFile, parser, through.obj(write), process.stdout, (err) => console.error(err))
+
+// With through.obj and regular pipes no premature close
+streamFile
+  .on("error", (err) => console.log(err))
+  .pipe(parser)
+  .on("error", (err) => console.log(err))
+  .pipe(through.obj(write))
+  .on("error", (err) => console.log(err))
+  .pipe(process.stdout)
+  .on("error", (err) => console.log(err))
+
 function write(row, enc, next) {
   next(null, JSON.stringify(row))
 }
